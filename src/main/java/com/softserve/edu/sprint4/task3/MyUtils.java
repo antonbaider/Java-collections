0109cd1f98a4fe8d7fa51938c9aef8f99aabf0da
antonbaider <email.b.anton@gmail.com>
2023-12-18 21:33:12 +0100

    done

--- src/main/java/com/softserve/edu/sprint4/task3/MyUtils.java
+++ src/main/java/com/softserve/edu/sprint4/task3/MyUtils.java
@@ -1,15 +1,50 @@
 package com.softserve.edu.sprint4.task3;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public class MyUtils {
-    // Code
-    public boolean listMapCompare(List<String> list, Map<String, String> map) {
-        // Code
-        //
-
-        return false; // TODO Update
-        //
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
-    
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+
+    public boolean listMapCompare(List<String> list, Map<String, String> map) {
+
+        if (list.isEmpty() && map.isEmpty()) {
+            return true;
+        }
+
+
+        if (list.isEmpty() || map.isEmpty()) {
+            return false;
+        }
+
+        for (String mapValue : map.values()) {
+            if (!list.contains(mapValue)) {
+                return false;
+            }
+        }
+
+        if (list.contains(null) || map.containsValue(null)) {
+            return true;
+        }
+
+        for (String s : list) {
+            for (String mapValue : map.values()) {
+                if (s.equals(mapValue)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
