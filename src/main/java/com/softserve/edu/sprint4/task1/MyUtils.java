9ae88495d7f44fab82813210c26c39672b6543d9
antonbaider <email.b.anton@gmail.com>
2023-12-18 20:21:08 +0100

    done

--- src/main/java/com/softserve/edu/sprint4/task1/MyUtils.java
+++ src/main/java/com/softserve/edu/sprint4/task1/MyUtils.java
@@ -6,12 +6,19 @@ import java.util.List;
 import java.util.Map;
 
 public class MyUtils {
-    // Code
     public Map<String, List<String>> createNotebook(Map<String, String> phones) {
-        // Code
-        //
-
-        return null;  // TODO Update
-        //
+        Map<String, List<String>> map = new HashMap<>();
+        for (Map.Entry<String, String> entry : phones.entrySet()) {
+            String contactName = entry.getKey();
+            String phoneNumber = entry.getValue();
+            if (map.containsKey(phoneNumber)) {
+                map.get(phoneNumber).add(contactName);
+            } else if (phoneNumber != null || contactName != null) {
+                List<String> contactNames = new ArrayList<>();
+                contactNames.add(contactName);
+                map.put(phoneNumber, contactNames);
+            }
+        }
+        return map;
     }
 }
