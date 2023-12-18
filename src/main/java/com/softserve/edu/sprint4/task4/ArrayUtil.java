c428e97ae541a4e2fe17e7375a38fc8b47df5cb6
antonbaider <email.b.anton@gmail.com>
2023-12-18 22:25:53 +0100

    done

--- src/main/java/com/softserve/edu/sprint4/task4/ArrayUtil.java
+++ src/main/java/com/softserve/edu/sprint4/task4/ArrayUtil.java
@@ -1,7 +1,11 @@
 package com.softserve.edu.sprint4.task4;
-
-class ArrayUtil {
-    // Write static method setAndReturn(...) here
-    //
-
+public class ArrayUtil {
+    @SuppressWarnings("unchecked")
+    public static <T> T setAndReturn(T[] array, T value, int index) {
+        if (index < 0 || index >= array.length) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        array[index] = value;
+        return (T) array[index];
+    }
 }
